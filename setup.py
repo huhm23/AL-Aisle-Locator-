e4992ad5bb1a3137065e90d85ab223952052a539
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-24 12:09:02 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bluetooth',
+      version='1.0',
+      description='CompE Stub Library',
+      author='Haelyne Uhm',
+      author_email='hu300@nyu.edu',
+      url='https://github.com/huhm23/setup',
+      py_modules=['bluetoothconnection'],
+     )
