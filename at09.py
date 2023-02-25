51caecf7e5400cc8ebdf30a7bf32f790104ea38f
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-25 13:43:02 -0500

    Update at09.py

--- at09.py
+++ at09.py
@@ -4,9 +4,9 @@ class bluetooth(DefaultDelegate):
     def _init_(self):
         DefaultDelegate.__init__(self)
     def scan_for_devices():
-            try:
-                ble_list = Scanner().scan(10.0)
-                for dev in ble_list:
-                    print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
-            except:
-                raise Exception("Error occured")
+        try:
+            ble_list = Scanner().scan(10.0)
+            for dev in ble_list:
+                print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+        except:
+            raise Exception("Error occured")
