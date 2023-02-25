2ecda279fb5b0e52866e51da79595136739ba486
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-25 12:03:11 -0500

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -1,2 +1,10 @@
-def info():
-  print("IT IS WORKING!!!!!!")
+from bluepy.btle import Scanner
+
+while True:
+    try:
+        #10.0 sec scanning
+        ble_list = Scanner().scan(10.0)
+        for dev in ble_list:
+            print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+    except:
+        raise Exception("Error occured")
