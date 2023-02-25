593b30b459dc7d455cc8392ceaa5c944100741ec
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-25 12:54:26 -0500

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -1,10 +1,14 @@
-from bluepy.btle import Scanner
+from bluepy.btle import Scanner, DefaultDelegate
 
-while True:
-    try:
-        #10.0 sec scanning
-        ble_list = Scanner().scan(10.0)
-        for dev in ble_list:
-            print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
-    except:
-        raise Exception("Error occured")
+class bluetooth(DefaultDelegate): 
+    def _init_(self):
+        DefaultDelegate.__init__(self)
+    def scan_for_devices():
+        while True:
+            try:
+                #10.0 sec scanning
+                ble_list = Scanner().scan(10.0)
+            for dev in ble_list:
+                print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+            except:
+                raise Exception("Error occured")
