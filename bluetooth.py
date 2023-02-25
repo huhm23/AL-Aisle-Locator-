0416c1d2f607075204df680093deee6a0f6e3a0a
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-25 12:56:15 -0500

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -8,7 +8,7 @@ class bluetooth(DefaultDelegate):
             try:
                 #10.0 sec scanning
                 ble_list = Scanner().scan(10.0)
-            for dev in ble_list:
-                print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+                for dev in ble_list:
+                    print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
             except:
                 raise Exception("Error occured")
