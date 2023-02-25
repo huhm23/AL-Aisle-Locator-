06c558b7834bca4f5c221e27c977533f7e0c1299
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-25 13:52:32 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,8 +2,11 @@ from at09 import bluetooth
 
 def scan_for_devices():
         try:
-            ble_list = Scanner().scan(10.0)
-            for dev in ble_list:
-                print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+                ble_list = Scanner().scan(10.0)
+                for dev in ble_list:
+                        print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
         except:
-            raise Exception("Error occured")
+                raise Exception("Error occured")
+
+                
+scan_for_devices()
