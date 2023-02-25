72dc1ea8b4bd5ceec2d376fc18de77ba76938d6e
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-25 13:48:55 -0500

    Update at09.py

--- at09.py
+++ at09.py
@@ -3,10 +3,3 @@ from bluepy.btle import Scanner, DefaultDelegate
 class bluetooth(DefaultDelegate): 
     def _init_(self):
         DefaultDelegate.__init__(self)
-    def scan_for_devices():
-        try:
-            ble_list = Scanner().scan(10.0)
-            for dev in ble_list:
-                print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
-        except:
-            raise Exception("Error occured")
