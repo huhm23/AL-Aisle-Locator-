dfe57f0269ceaa054a4bfe1ec3e97403a81d4c60
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-26 18:00:16 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ def scan_for_devices():
                 try:
                         ble_list = Scanner().scan(10.0)
                         for dev in ble_list:
-                                print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+                                print("mac: {}; rssi: {}".format(dev.addr,dev.rssi))
                 except:
                         raise Exception("Error occured")
 scan_for_devices()
