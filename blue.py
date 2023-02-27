a599f2b02cbaefea4eb846c757f4e99b48c04677
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-27 13:55:22 -0500

    Update blue.py

--- blue.py
+++ blue.py
@@ -2,8 +2,13 @@ from bluepy.btle import Scanner
 
 while True:
         try:
-                ble_list = Scanner().scan(5.0)
+        #10.0 sec scanning
+                ble_list = Scanner().scan(10.0)
+                MR_list = []
                 for dev in ble_list:
-                        MR_list = []
-                        MR_list.append("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
-                        print(MR_list)
+                        if dev.addr == "90:e2:02:b1:96:c3":
+                                MR_list.append("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
+        except:
+                raise Exception("Error occured")
+                
+print(MR_list)
