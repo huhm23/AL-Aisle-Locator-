01c92e3ac5238d0528ee8cddb9ca22ef33da5ac5
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-27 13:37:41 -0500

    Update blue.py

--- blue.py
+++ blue.py
@@ -3,4 +3,6 @@ from bluepy.btle import Scanner
 while True:
         try:
                 ble_list = Scanner().scan(10.0)
-                print(ble_list)
+                for dev in ble_list:
+                        print(dev)
+                #print(ble_list)
