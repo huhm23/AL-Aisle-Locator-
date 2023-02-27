d81d11d0f5897f61357170a888d42913c6e50243
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-27 14:11:39 -0500

    Update blue.py

--- blue.py
+++ blue.py
@@ -8,7 +8,9 @@ while True:
                 for dev in ble_list:
                         if dev.addr == "90:e2:02:b1:96:c3":
                                 MR_list.append("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
-                                print(MR_list)
+                        elif dev.addr == "d0:b5:c2:96:6b:f3":
+                                MR_list.append("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
+                        print(MR_list)
         except:
                 raise Exception("Error occured")
                 
