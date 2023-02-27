644459bf7226377ba3fdb00e4ee055962363ae45
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-26 21:04:48 -0500

    Update blueloc.py

--- blueloc.py
+++ blueloc.py
@@ -7,6 +7,6 @@ while True:
                 for dev in ble_list:
            # print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
                         if dev.addr == "90:E2:02:B1:96:C3":
-                                print("mac: ; rssi: ".format(dev.addr, dev.rssi)
-                        else:
-                                raise Exception("Error occured")
+                                print("mac: ; rssi: ".format(dev.addr, dev.rssi))
+         except:
+                 raise Exception("Error occured")
