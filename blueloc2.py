6fe78bc384f9c71e878682aa39f20b8e8d83bca3
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-27 14:24:08 -0500

    Update blueloc2.py

--- blueloc2.py
+++ blueloc2.py
@@ -1,8 +1,8 @@
 from bluepy.btle import Scanner
 
+ble_list = Scanner().scan(10.0)
 while True:
         try:
-                ble_list = Scanner().scan(10.0)
                 for dev in ble_list:
                         if dev.addr == "90:e2:02:b1:96:c3":
                                 print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
