85ed48e5b30bbef6fb9c12d222eacf9f0e31506f
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-27 13:52:03 -0500

    Update blue.py

--- blue.py
+++ blue.py
@@ -3,7 +3,7 @@ from bluepy.btle import Scanner
 while True:
         try:
                 ble_list = Scanner().scan(5.0)
-                MR_list = []
                 for dev in ble_list:
+                        MR_list = []
                         MR_list.append("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
                 print(MR_list)
