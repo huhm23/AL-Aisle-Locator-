cdf639731498f3783f1a19562df79ba3c44a47fa
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-26 21:01:30 -0500

    Create blueloc.py

---
+++ blueloc.py
@@ -0,0 +1,12 @@
+from bluepy.btle import Scanner
+
+while True:
+        try:
+        #10.0 sec scanning
+                ble_list = Scanner().scan(10.0)
+                for dev in ble_list:
+           # print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+                        if dev.addr == "90:E2:02:B1:96:C3":
+                                print("mac: ; rssi: ".format(dev.addr, dev.rssi)
+                        else:
+                                raise Exception("Error occured")
