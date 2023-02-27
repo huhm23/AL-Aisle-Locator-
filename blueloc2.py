b21242e1a6e545bb21800dc0955162b24e9d0e53
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-27 13:14:17 -0500

    Create blueloc2.py

---
+++ blueloc2.py
@@ -0,0 +1,10 @@
+from bluepy.btle import Scanner
+
+while True:
+        try:
+                ble_list = Scanner().scan(10.0)
+                for dev in ble_list:
+                        if dev.addr == "90:e2:02:b1:96:c3" or "d0:b5:c2:96:6b:f3":
+                                print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
+        except:
+                raise Exception("Error occured")
