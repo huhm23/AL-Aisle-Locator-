5d700a013c135056401d575ad9b3d4f4583c9349
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-02-27 13:22:46 -0500

    Update blueloc2.py

--- blueloc2.py
+++ blueloc2.py
@@ -4,7 +4,9 @@ while True:
         try:
                 ble_list = Scanner().scan(10.0)
                 for dev in ble_list:
-                        if dev.addr == "90:e2:02:b1:96:c3" or "d0:b5:c2:96:6b:f3":
+                        if dev.addr == "90:e2:02:b1:96:c3"
+                                print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
+                        elif dev.addr == "d0:b5:c2:96:6b:f3":
                                 print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
                         else:
                                 pass 
