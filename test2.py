7b085c73d28521dc62d9fda239870aa6cca448e4
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-03-01 12:57:26 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,12 +1,17 @@
 from bluepy.btle import Scanner
 
+my_list = []
 try:
   ble_list = Scanner().scan(10.0)
   for dev in ble_list:
-    print(dev.addr)
     if dev.addr == "90:e2:02:b1:96:c3" or dev.addr == "d0:b5:c2:96:6b:f3":
-      print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
+      my_list.append(dev)
+      #if dev.rssi 
+      #print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
                         #else:
                                 #pass 
 except:
   raise Exception("Error occured")
+  
+for dev in my_list:
+  print(f"mac: {dev.addr} ; rssi: {dev.rssi}")
