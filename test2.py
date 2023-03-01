d4b39837b7c3e213c80b7c73d2f242149f6b8bc6
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-03-01 12:43:34 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,12 @@
 from bluepy import btle
 
-b1 = btle.Peripheral("90:e2:02:b1:96:c3")
-
-print(b1.rssi)
+try:
+  ble_list = Scanner().scan(10.0)
+  for dev in ble_list:
+    print(dev.addr)
+                        #if dev.addr == "90:e2:02:b1:96:c3" or "d0:b5:c2:96:6b:f3":
+                                #print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
+                        #else:
+                                #pass 
+except:
+  raise Exception("Error occured")
