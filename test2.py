b5498afa68f73c38b3b198315062605108cd9a59
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-03-01 12:48:33 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,8 +4,8 @@ try:
   ble_list = Scanner().scan(10.0)
   for dev in ble_list:
     print(dev.addr)
-                        #if dev.addr == "90:e2:02:b1:96:c3" or "d0:b5:c2:96:6b:f3":
-                                #print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
+    if dev.addr == "90:e2:02:b1:96:c3" or dev.addr == "d0:b5:c2:96:6b:f3":
+      print("mac: {} ; rssi: {}".format(dev.addr, dev.rssi))
                         #else:
                                 #pass 
 except:
