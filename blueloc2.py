6f5120421b7275d790cfefe9ddf87f8dc03386b8
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-03-01 20:09:05 -0500

    Update blueloc2.py

--- blueloc2.py
+++ blueloc2.py
@@ -6,8 +6,12 @@ from bluepy.btle import Scanner
 #B4: ec:11:27:54:4b:42
 #B5: b4:99:4c:79:cc:a1
 
+# create list for my bluetooth devices
 my_list = []
+# create list for devices that are scanned
 ble_list = Scanner().scan(10.0)
+
+# if the device MAC address equals the MAC address of my bluetooth devices the put them in my_list
 try:
   for dev in ble_list:
     if dev.addr == "90:e2:02:b1:96:c3" or dev.addr == "d0:b5:c2:96:6b:f3" or dev.addr == "78:a5:04:90:82:69" \
@@ -16,15 +20,18 @@ try:
 except:
   raise Exception("Error occured")
 
-
+# max (never can reach) number for the RSSI
 max_num = -10000
+# max (never can reach) number for the index
 max_index = -1
-  
+
+# check which index has the lowest RSSI number:
 for i in range(len(my_list)):
   if my_list[i].rssi >= max_num: 
     max_num = my_list[i].rssi
     max_index = i
 
+# compare the address to the bluetooth addresses and print the statement of the one you are near
 if my_list[max_index].addr == "90:e2:02:b1:96:c3":
   print("You are near bluetooth 1")
 elif my_list[max_index].addr == "d0:b5:c2:96:6b:f3":
