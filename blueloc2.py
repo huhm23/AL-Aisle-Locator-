e71e35bdf9998baf9e10ef4dce8c593e4d3493c2
huhm23 <124328719+huhm23@users.noreply.github.com>
2023-03-01 19:52:41 -0500

    Update blueloc2.py

--- blueloc2.py
+++ blueloc2.py
@@ -1,10 +1,17 @@
 from bluepy.btle import Scanner
 
+#B1: 90:e2:02:b1:96:c3
+#B2: d0:b5:c2:96:6b:f3
+#B3: 78:a5:04:90:82:69
+#B4: ec:11:27:54:4b:42
+#B5: b4:99:4c:79:cc:a1
+
 my_list = []
 try:
   ble_list = Scanner().scan(10.0)
   for dev in ble_list:
-    if dev.addr == "90:e2:02:b1:96:c3" or dev.addr == "d0:b5:c2:96:6b:f3":
+    if dev.addr == "90:e2:02:b1:96:c3" or dev.addr == "d0:b5:c2:96:6b:f3" or dev.addr == "78:a5:04:90:82:69" \
+    or dev.addr == "ec:11:27:54:4b:42" or dev.addr == "b4:99:4c:79:cc:a1":
       my_list.append(dev)
 except:
   raise Exception("Error occured")
@@ -14,14 +21,20 @@ max_num = -10000
 max_index = -1
   
 for i in range(len(my_list)):
-  if my_list[i].rssi >= max_num:
+  if my_list[i].rssi >= max_num: 
     max_num = my_list[i].rssi
     max_index = i
 
 if my_list[max_index].addr == "90:e2:02:b1:96:c3":
   print("You are near bluetooth 1")
 elif my_list[max_index].addr == "d0:b5:c2:96:6b:f3":
-  print("You are near bluetooth 2") 
+  print("You are near bluetooth 2")
+elif my_list[max_index].addr == "78:a5:04:90:82:69":
+  print("You are near bluetooth 2")
+elif my_list[max_index].addr == "ec:11:27:54:4b:42":
+  print("You are near bluetooth 2")
+else:
+  print("You are near bluetooth 3")
     
 
 print(f"mac: {my_list[max_index].addr} ; rssi: {my_list[max_index].rssi}")
